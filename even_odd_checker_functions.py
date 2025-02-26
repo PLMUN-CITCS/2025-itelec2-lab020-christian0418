a55bc7496e31220b0a3f150f007157d42a0daadc
christian0418 <tanchristian_bsit@plmun.edu.ph>
2025-02-26 12:07:51 +0800

    even_odd_checker_functions.py

--- even_odd_checker_functions.py
+++ even_odd_checker_functions.py
@@ -1,8 +1,8 @@
-def check_number(num):
-    if num % 2 == 0:
-        print(f"{num} is an Even number.")
+def check_number_type(number):
+    if number % 2 == 0:
+        print(f"{number} is an Even number.")
     else:
-        print(f"{num} is an Odd number.")
+        print(f"{number} is an Odd number.")
 
 num = int(input("Enter an integer: "))
-check_number(num)
+check_number_type(num)
